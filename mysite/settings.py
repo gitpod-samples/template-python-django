c99151054beed974925c5f383d6aeefa91b8e676
stivkh <stivkh@me.com>
2021-10-15 07:56:48 +0000

    commit main view

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
+    'django.contrib.staticfiles',
     'first_app',
 ]
 
