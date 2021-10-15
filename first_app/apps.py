d07c01aafea88151b06e3d4878238431e9ebb19f
stivkh <stivkh@me.com>
2021-10-15 07:32:25 +0000

    commit on main

---
+++ first_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FirstAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'first_app'
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from first_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.index, name='index'),
 ]
